1e56badac12074b4072702d38a23c63574ce8b6a
Tusar <tusarahmed141@gmail.com>
2022-08-10 19:42:18 +0600

    Exploring a awesome line chart

--- tailwind-pricing-club/src/Components/MyChart/MyChart.js
+++ tailwind-pricing-club/src/Components/MyChart/MyChart.js
@@ -1,49 +1,46 @@
 import React from "react";
-import {
-  LineChart,
-  Line,
-} from "recharts";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from "recharts";
 
 const data = [
   {
-    name: 'Page A',
-    uv: 4000,
+    name: "Product A",
+    sales: 4000,
     price: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
-    uv: 3000,
+    name: "Product B",
+    sales: 3000,
     price: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
-    uv: 2000,
+    name: "Product C",
+    sales: 2000,
     price: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
-    uv: 2780,
+    name: "Product D",
+    sales: 2780,
     price: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
-    uv: 1890,
+    name: "Product E",
+    sales: 1890,
     price: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
-    uv: 2390,
+    name: "Product F",
+    sales: 2390,
     price: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
-    uv: 3490,
+    name: "Product G",
+    sales: 3490,
     price: 4300,
     amt: 2100,
   },
@@ -53,12 +50,33 @@ const MyChart = () => {
   return (
     <div className="w-full min-h-screen bg-gradient-to-r from-blue-100 via-purple-200 to-blue-100 ">
       <div className="container mx-auto">
-        <LineChart width={500} height={300} data={data}>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
           <Line
             type="monotone"
             dataKey="price"
             stroke="#8884d8"
-            strokeWidth={2}
+            strokeDasharray="5 5"
+          />
+          <Line
+            type="monotone"
+            dataKey="sales"
+            stroke="#82ca9d"
+            strokeDasharray="3 4 5 2"
           />
         </LineChart>
       </div>
