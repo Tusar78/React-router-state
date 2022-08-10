647fd9a18822cad88360523403a4f58777a18d1b
Tusar <tusarahmed141@gmail.com>
2022-08-10 16:37:31 +0600

    trying to explore bootstrap carousel

--- react-with-bootstrap/src/App.js
+++ react-with-bootstrap/src/App.js
@@ -1,11 +1,13 @@
 import Accordion2 from "./Components/Accordion2/Accordion2";
 import Buttons2 from "./Components/Buttons2/Buttons2";
+import Carosel from "./Components/Carosel/Carosel";
 
 function App() {
   return (
     <div className="container">
       <Accordion2 />
       <Buttons2 />
+      <Carosel />
     </div>
   );
 }
