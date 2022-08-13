06bf48706ae5a2bc27aa4a15cd88215590923a6b
Tusar <tusarahmed141@gmail.com>
2022-08-13 12:13:03 +0600

    Added disabled button

--- heels-villa-practice/src/Cart/Cart.js
+++ heels-villa-practice/src/Cart/Cart.js
@@ -3,21 +3,35 @@ import "./Cart.css";
 import { IoTrashBin } from "react-icons/io5";
 
 const Cart = ({ cart, products, handleClearCart }) => {
+  const [offer, setOffer] = useState(false);
+  const [offerProduct, setOfferProduct] = useState({});
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      setOffer(true)
+    }
+  }, [cart.length])
+
+  const handleOffer = () => {
+    const randomNumber = Math.floor(Math.random() * products.length);
+    const item = products[randomNumber];
+    setOfferProduct(item);
+  };
   return (
-    <div className='cart'>
-      <div className='cart-header'>
+    <div className="cart">
+      <div className="cart-header">
         <h1>Order Summery</h1>
         <button
           onClick={handleClearCart}
-          className='remove-button'
-          title='Clear Cart'
+          className="remove-button"
+          title="Clear Cart"
         >
-          <IoTrashBin color='white' size={20} />
+          <IoTrashBin color="white" size={20} />
         </button>
       </div>
       {cart.map((product, index) => (
-        <div key={index} className='cart-item'>
-          <img src={product.pairImage} alt='' />
+        <div key={index} className="cart-item">
+          <img src={product.pairImage} alt="" />
           <div>
             <p>
               {product.name} {product.color}
@@ -26,6 +40,22 @@ const Cart = ({ cart, products, handleClearCart }) => {
           </div>
         </div>
       ))}
+
+      <p>By one get one free</p>
+      <button className={offer ? "offer-button" : "offer-button-disabled"} disabled={!offer}  onClick={handleOffer}>
+        Get one for me
+      </button>
+      {Object.keys(offerProduct).length > 0 && (
+        <div className="cart-item">
+          <img src={offerProduct.pairImage} alt="" />
+          <div>
+            <p>
+              {offerProduct.name} {offerProduct.color}
+            </p>
+            <p>$ {offerProduct.price}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
