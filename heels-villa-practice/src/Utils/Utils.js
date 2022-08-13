da415f69bf25d0e2e79218b93033a5d6eee5973a
Tusar <tusarahmed141@gmail.com>
2022-08-13 15:38:10 +0600

    Added value on local storage successfully

--- heels-villa-practice/src/Utils/Utils.js
+++ heels-villa-practice/src/Utils/Utils.js
@@ -1,5 +1,26 @@
+const getLocalStorage = () => {
+  const getJSON = localStorage.getItem('shopping_cart');
+  const getItem = JSON.parse(getJSON);
+  return getItem;
+} 
+
+console.log(getLocalStorage());
+
 const addToLocalStorage = id => {
-  const shoppingCart = {};
+  const exits = getLocalStorage();
+  let shoppingCart = {};
+  if (!exits) {
+    shoppingCart[id] = 1;
+  } else {
+    shoppingCart = exits;
+    if (shoppingCart[id]) {
+      const newCart = shoppingCart[id] + 1;
+      shoppingCart[id] = newCart;
+    } else {
+      shoppingCart[id] = 1;
+    }
+  }
+
   localStorage.setItem('shopping_cart', JSON.stringify(shoppingCart))
 }
 
