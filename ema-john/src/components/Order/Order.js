e89177b2ab20673d05a50ba20489b0fdee4aa34c
Tusar <tusarahmed141@gmail.com>
2022-08-17 23:28:15 +0600

    LAst update

--- ema-john/src/components/Order/Order.js
+++ ema-john/src/components/Order/Order.js
@@ -1,18 +1,31 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import useProduct from "../../hooks/useProducts";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Order = () => {
   const [products, setProducts] = useProduct();
   const [cart, setCart] = useCart(products);
+
+  const handleRemoveProduct = (product) => {
+    console.log(product);
+    const rest = cart.filter(pd => pd.id !== product.id);
+    setCart(rest)
+    removeFromDb(product.id)
+  };
+
   return (
     <div className="shop-container">
-      <div className="products-container">
-        {
-          cart.map(product => <ReviewItem key={product.id} product={product} />)
-        }
+      <div className="m-8 grid gap-4">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveProduct={handleRemoveProduct}
+          />
+        ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart} />
--- ema-john/src/components/ReviewItem/ReviewItem.js
+++ ema-john/src/components/ReviewItem/ReviewItem.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const ReviewItem = ({ product }) => {
+const ReviewItem = ({ product, handleRemoveProduct }) => {
   const { name, price, shipping, quantity, img } = product;
   return (
-    <div>
-      <img style={{width: '80px'}} src={img} alt={name} />
-      <h4>{name}</h4>
-      <p>Price: {price}</p>
-      <p>Shipping: {shipping}</p>
-      <p>Qyt: {quantity}</p>
+    <div className="border p-4 flex rounded-md items-center w-full justify-between">
+      <div className="flex space-x-4">
+        <img style={{ width: "80px" }} src={img} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>Price: {price}</p>
+          <p>Shipping: {shipping}</p>
+          <p>Qyt: {quantity}</p>
+        </div>
+      </div>
+      <div>
+        <button className="w-12 h-12 rounded-full shadow-md bg-white flex items-center justify-center" onClick={() => handleRemoveProduct(product)}>
+          <FontAwesomeIcon className="text-red-400" icon={faTrash} />
+        </button>
+      </div>
     </div>
   );
 };
