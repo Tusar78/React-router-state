51f784a3b70cd5b04f09558cac531163fdd25cab
Tusar <tusarahmed141@gmail.com>
2022-08-20 12:16:35 +0600

    Explore awesome context API

--- tshirt-mania/src/Components/Grandpa/Grandpa.js
+++ tshirt-mania/src/Components/Grandpa/Grandpa.js
@@ -1,26 +1,36 @@
-import React, { useState } from 'react';
-import Aunty from '../Aunty/Aunty';
-import Father from '../Father/Father';
-import Uncle from '../Uncle/Uncle';
+import React, { createContext, useState } from "react";
+import Aunty from "../Aunty/Aunty";
+import Father from "../Father/Father";
+import Uncle from "../Uncle/Uncle";
+
+export const RingContext = createContext("Ring");
 
 const Grandpa = () => {
   const [myHouse, setMyHouse] = useState(0);
   const handleHouse = () => {
     const newHouse = myHouse + 1;
-    setMyHouse(newHouse)
-  }
+    setMyHouse(newHouse);
+  };
+
+  const ornament = "Diamond Ring!";
+
   const house = <p>There are {myHouse} house</p>;
+
   return (
-    <div className='max-w-screen-lg mx-auto mt-6 border border-amber-400 p-4'>
-      <h2>Grandpa</h2>
-      <button className='btn btn-accent' onClick={handleHouse}>Increased House</button>
-      {house}
-      <div className='m-2 p-2 grid grid-cols-3 gap-4'>
-        <Father house={house} />
-        <Uncle house={house} />
-        <Aunty house={house} />
+    <RingContext.Provider value={ornament}>
+      <div className="max-w-screen-lg mx-auto mt-6 border border-amber-400 p-4">
+        <h2>Grandpa</h2>
+        <button className="btn btn-accent" onClick={handleHouse}>
+          Increased House
+        </button>
+        {house}
+        <div className="m-2 p-2 grid grid-cols-3 gap-4">
+          <Father house={house} />
+          <Uncle house={house} />
+          <Aunty house={house} />
+        </div>
       </div>
-    </div>
+    </RingContext.Provider>
   );
 };
 
--- tshirt-mania/src/Components/MySelf/MySelf.js
+++ tshirt-mania/src/Components/MySelf/MySelf.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const MySelf = ({house}) => {
+  const myGift = useContext(RingContext)
   return (
     <div className='border border-pink-400 p-4'>
       <h5>My Self</h5>
       {house}
+      <p className='text-white bg-purple-600 font-semibold px-2'>{myGift}</p>
     </div>
   );
 };
